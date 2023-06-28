da77079869497da47c1807aadc6c24b4c1759bbf
Chetan A <chetan.agrawal@thoughtspot.com>
2023-06-28 23:37:40 +0530

    added custom chart config in the custom column chart

--- example/custom-bar-chart/custom-chart.ts
+++ example/custom-bar-chart/custom-chart.ts
@@ -8,59 +8,65 @@
  * Copyright: ThoughtSpot Inc. 2023
  */
 
-import Chart from 'chart.js/auto';
-import _ from 'lodash';
 import {
-    CustomChartContext,
-    getChartContext,
     ChartColumn,
-    ChartToTSEvent,
-    ChartModel,
     ChartConfig,
+    ChartModel,
+    ChartToTSEvent,
     ColumnType,
+    CustomChartContext,
     DataArray,
+    getChartContext,
     PointVal,
     Query,
-} from '../../src';
+} from '@thoughtspot/ts-chart-sdk';
+import Chart from 'chart.js/auto';
+import _ from 'lodash';
+
+let globalChartReference: Chart;
 
 function getDataForColumn(column: ChartColumn, dataArr: DataArray[]) {
     const colId = column.id;
-    const idx = _.findIndex(dataArr, (dataObj: any) => dataObj.columnId === colId);
+    const idx = _.findIndex(
+        dataArr,
+        (dataObj: any) => dataObj.columnId === colId,
+    );
     return dataArr[idx].dataValue;
 }
 
-function getDataModel(chartModel: ChartModel) {
-    const configDimensions = chartModel.config?.chartConfig?.[0].dimensions ?? [];
-    const dataArr = chartModel.data?.[0].data ?? [];
-
+function getColumnDataModel(configDimensions, dataArr, type) {
     // this should be handled in a better way
     const xAxisColumns = configDimensions?.[0].columns ?? [];
     const yAxisColumns = configDimensions?.[1].columns ?? [];
 
     return {
         getLabels: () => getDataForColumn(xAxisColumns[0], dataArr),
-        getDatasets: () => _.map(yAxisColumns, (col, idx) => ({
-            label: col.name,
-            data: getDataForColumn(col, dataArr),
-            yAxisID: `y${idx.toString()}`,
-        })),
-        getScales: () => _.reduce(
-            yAxisColumns,
-            (obj: any, _val, idx) => {
-                obj[`y${idx.toString()}`] = {
-                    grid: {
-                        display: true,
-                    },
-                    position: idx === 0 ? 'left' : 'right',
-                    title: {
-                        display: true,
-                        text: `${_val.name}`,
-                    },
-                };
-                return obj;
-            },
-            {},
-        ),
+        getDatasets: () =>
+            _.map(yAxisColumns, (col, idx) => ({
+                label: col.name,
+                data: getDataForColumn(col, dataArr),
+                yAxisID: `${type}-y${idx.toString()}`,
+                type: `${type}`,
+            })),
+        getScales: () =>
+            _.reduce(
+                yAxisColumns,
+                (obj: any, _val, idx: number) => {
+                    // eslint-disable-next-line no-param-reassign
+                    obj[`${type}-y${idx.toString()}`] = {
+                        grid: {
+                            display: true,
+                        },
+                        position: idx === 0 ? 'left' : 'right',
+                        title: {
+                            display: true,
+                            text: `${_val.name}`,
+                        },
+                    };
+                    return obj;
+                },
+                {},
+            ),
         getPointDetails: (xPos: number, yPos: number): PointVal[] => [
             {
                 columnId: xAxisColumns[0].id,
@@ -74,29 +80,39 @@ function getDataModel(chartModel: ChartModel) {
     };
 }
 
+function getDataModel(chartModel) {
+    // column chart model
+    const columnChartModel = getColumnDataModel(
+        chartModel.config?.chartConfig?.[0].dimensions ?? [],
+        chartModel.data?.[0].data ?? [],
+        'bar',
+    );
+
+    return columnChartModel;
+}
+
 function getParsedEvent(evt: any) {
-    return _.pick(evt.native, [
-        'x',
-        'y',
-        'clientX',
-        'clientY',
-        'pageX',
-        'pageY',
-        'screenX',
-        'screenY',
-    ]);
+    return _.pick(evt.native, ['clientX', 'clientY']);
 }
 
 function render(ctx: CustomChartContext) {
     const chartModel = ctx.getChartModel();
     const dataModel = getDataModel(chartModel);
 
+    if (!dataModel) {
+        return;
+    }
+
     try {
-        const chart = new Chart(document.getElementById('chart') as any, {
+        const canvas = document.getElementById('chart') as any;
+        // clear canvas.
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+        globalChartReference = new Chart(canvas as any, {
             type: 'bar',
             data: {
                 labels: dataModel.getLabels(),
-                datasets: dataModel.getDatasets(),
+                datasets: dataModel.getDatasets() as any,
             },
             options: {
                 scales: dataModel.getScales(),
@@ -107,12 +123,9 @@ function render(ctx: CustomChartContext) {
                     intersect: true,
                 },
                 onClick: (e: any) => {
-                    // console.log(e);
-                    // console.log(e.chart.getActiveElements()[0]);
                     const activeElement = e.chart.getActiveElements()[0];
                     const dataX = activeElement.index;
                     const dataY = activeElement.datasetIndex;
-                    // const canvasPosition = getRelativePosition(e, e.chart);
 
                     console.log(
                         'ChartPoint',
@@ -135,7 +148,10 @@ function render(ctx: CustomChartContext) {
     }
 }
 
-const renderChart = async (ctx: CustomChartContext):Promise<void> => {
+const renderChart = async (ctx: CustomChartContext): Promise<void> => {
+    if (globalChartReference) {
+        globalChartReference.destroy();
+    }
     try {
         ctx.emitEvent(ChartToTSEvent.RenderStart, null);
         render(ctx);
@@ -154,12 +170,18 @@ const renderChart = async (ctx: CustomChartContext):Promise<void> => {
         getDefaultChartConfig: (chartModel: ChartModel): ChartConfig[] => {
             const cols = chartModel.columns;
 
-            const measureColumns = _.filter(cols, (col) => col.type === ColumnType.MEASURE);
+            const measureColumns = _.filter(
+                cols,
+                (col) => col.type === ColumnType.MEASURE,
+            );
 
-            const attributeColumns = _.filter(cols, (col) => col.type === ColumnType.ATTRIBUTE);
+            const attributeColumns = _.filter(
+                cols,
+                (col) => col.type === ColumnType.ATTRIBUTE,
+            );
 
             const axisConfig: ChartConfig = {
-                key: 'default',
+                key: 'column',
                 dimensions: [
                     {
                         key: 'x',
@@ -175,16 +197,51 @@ const renderChart = async (ctx: CustomChartContext):Promise<void> => {
         },
         getQueriesFromChartConfig: (
             chartConfig: ChartConfig[],
-        ): Array<Query> => chartConfig.map((config: ChartConfig): Query => _.reduce(
-            config.dimensions,
-            (acc: Query, dimension) => ({
-                queryColumns: [...acc.queryColumns, ...dimension.columns],
-            }),
-            {
-                queryColumns: [],
-            } as Query,
-        )),
+        ): Array<Query> => {
+            const queries = chartConfig.map(
+                (config: ChartConfig): Query =>
+                    _.reduce(
+                        config.dimensions,
+                        (acc: Query, dimension) => ({
+                            queryColumns: [
+                                ...acc.queryColumns,
+                                ...dimension.columns,
+                            ],
+                        }),
+                        {
+                            queryColumns: [],
+                        } as Query,
+                    ),
+            );
+            return queries;
+        },
         renderChart: (ctx) => renderChart(ctx),
+        chartConfigEditorDefinition: [
+            {
+                key: 'column',
+                label: 'Custom Column',
+                descriptionText:
+                    'X Axis can only have attributes, Y Axis can only have measures, Color can only have attributes. ' +
+                    'Should have just 1 column in Y axis with colors columns.',
+                columnSections: [
+                    {
+                        key: 'x',
+                        label: 'Custom X Axis',
+                        allowAttributeColumns: true,
+                        allowMeasureColumns: false,
+                        allowTimeSeriesColumns: true,
+                        maxColumnCount: 1,
+                    },
+                    {
+                        key: 'y',
+                        label: 'Custom Y Axis',
+                        allowAttributeColumns: false,
+                        allowMeasureColumns: true,
+                        allowTimeSeriesColumns: false,
+                    },
+                ],
+            },
+        ],
     });
 
     renderChart(ctx);
