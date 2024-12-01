34a437a6a30cde6802476d0e7334b22a8756d770
yashvardhan-nehra <yashvardhan.nehra@thoughtspot.com>
2024-12-02 03:10:34 +0530

    SCAL-192739 Added Number Formatting Utils

---
+++ src/utils/number-formatting/formatting-utils.ts
@@ -0,0 +1,269 @@
+/**
+ * @file: Formatting Utils
+ *
+ * @author Yashvardhan Nehra <yashvardhan.nehra@thoughtspot.com>
+ *
+ * Copyright: ThoughtSpot Inc. 2024
+ */
+
+import _ from 'lodash';
+import {
+    ColumnFormat,
+    CurrencyFormat,
+    CurrencyFormatType,
+} from '../../types/answer-column.types';
+import { Maybe } from '../../types/common.types';
+import {
+    CategoryType,
+    CurrencyFormatConfig,
+    FormatConfig,
+    NegativeValueFormat,
+    NumberFormatConfig,
+    Unit,
+} from '../../types/number-formatting.types';
+import { getDefaultCurrencyCode } from './globalize-setup';
+
+interface FormatterConfig {
+    unitDetails: Unit;
+    decimalDetails: number;
+    shouldRemoveTrailingZeros: boolean;
+}
+
+export const PROTO_TO_NEGATIVE_VALUE_FORMAT = {
+    1: NegativeValueFormat.PrefixDash,
+    2: NegativeValueFormat.SuffixDash,
+    3: NegativeValueFormat.BracesNodash,
+};
+
+/**
+ * Constants for unit conversions and suffixes.
+ */
+export const UNITS_TO_DIVIDING_FACTOR: Record<Unit, number> = {
+    [Unit.None]: 1,
+    [Unit.Thousands]: 1000,
+    [Unit.Million]: 1000 * 1000,
+    [Unit.Billion]: 1000 * 1000 * 1000,
+    [Unit.Trillion]: 1000 * 1000 * 1000 * 1000,
+    [Unit.Auto]: 1,
+};
+
+export const PROTO_TO_UNITS = {
+    1: Unit.None,
+    2: Unit.Thousands,
+    3: Unit.Million,
+    4: Unit.Billion,
+    5: Unit.Trillion,
+    6: Unit.Auto,
+};
+
+/**
+ * Default strings for placeholders.
+ */
+const strings = {
+    NULL_VALUE_PLACEHOLDER_LABEL: '{Null}',
+    EMPTY_VALUE_PLACEHOLDER_LABEL: '{Empty}',
+};
+
+export const UNITS_TO_SUFFIX: Record<Unit, string> = {
+    [Unit.None]: '',
+    [Unit.Thousands]: 'K',
+    [Unit.Million]: 'M',
+    [Unit.Billion]: 'B',
+    [Unit.Trillion]: 'T',
+    [Unit.Auto]: '',
+};
+
+const DEFAULT_DECIMAL_PRECISION = 2;
+
+/**
+ * Formats negative values according to the specified format.
+ *
+ * @param formattedValue - The formatted value to modify.
+ * @param negativeFormat - The desired negative value format.
+ * @returns The formatted negative value.
+ */
+export const formatNegativeValue = (
+    formattedValue: string,
+    negativeFormat?: Maybe<NegativeValueFormat>,
+): string => {
+    if (typeof negativeFormat === 'number') {
+        // eslint-disable-next-line no-param-reassign
+        negativeFormat = PROTO_TO_NEGATIVE_VALUE_FORMAT[negativeFormat];
+    }
+    switch (negativeFormat) {
+        case NegativeValueFormat.PrefixDash:
+            return `-${formattedValue}`;
+        case NegativeValueFormat.SuffixDash:
+            return `${formattedValue}-`;
+        case NegativeValueFormat.BracesNodash:
+            return `(${formattedValue})`;
+        default:
+            return `-${formattedValue}`;
+    }
+};
+
+/**
+ * Determines the appropriate unit for auto-scaling values.
+ *
+ * @param value - The numeric value to evaluate.
+ * @returns The appropriate unit (e.g., Thousand, Million, etc.).
+ */
+export const getAutoUnit = (value: number): Unit => {
+    if (value >= UNITS_TO_DIVIDING_FACTOR[Unit.Trillion]) {
+        return Unit.Trillion;
+    }
+    if (value >= UNITS_TO_DIVIDING_FACTOR[Unit.Billion]) {
+        return Unit.Billion;
+    }
+    if (value >= UNITS_TO_DIVIDING_FACTOR[Unit.Million]) {
+        return Unit.Million;
+    }
+    if (value >= UNITS_TO_DIVIDING_FACTOR[Unit.Thousands]) {
+        return Unit.Thousands;
+    }
+    return Unit.None;
+};
+
+/**
+ * Retrieves the locale name based on the provided currency format.
+ *
+ * @param currencyFormat - The currency format configuration.
+ * @returns The locale name as a string.
+ */
+export const getLocaleName = (currencyFormat: CurrencyFormat): string => {
+    let locale = getDefaultCurrencyCode();
+    if (currencyFormat.type === CurrencyFormatType.ISO_CODE) {
+        locale = currencyFormat.isoCode;
+    }
+    return locale;
+};
+
+/**
+ * Generates a default format configuration based on the column settings.
+ *
+ * @param columnFormatConfig - The column-specific formatting settings.
+ * @returns The default format configuration.
+ */
+export const defaultFormatConfig = (
+    columnFormatConfig: ColumnFormat,
+): FormatConfig => {
+    if (columnFormatConfig?.pattern) {
+        return {
+            __typename: 'FormatConfig',
+            category: CategoryType.Custom,
+            isCategoryEditable: true,
+            customFormatConfig: {
+                __typename: 'CustomFormatConfig',
+                format: columnFormatConfig?.pattern,
+            },
+        };
+    }
+    if (columnFormatConfig?.currencyFormat) {
+        const locale = getLocaleName(columnFormatConfig?.currencyFormat);
+        const currencyFormatConfig: CurrencyFormatConfig = {
+            __typename: 'CurrencyFormatConfig',
+            decimals: 0,
+            locale,
+            removeTrailingZeroes: false,
+            toSeparateThousands: true,
+            unit: Unit.Auto,
+        };
+        const formatConfig: FormatConfig = {
+            __typename: 'FormatConfig',
+            category: CategoryType.Currency,
+            isCategoryEditable: true,
+            currencyFormatConfig,
+        };
+        return formatConfig;
+    }
+    const numberFormatConfig: NumberFormatConfig = {
+        __typename: 'NumberFormatConfig',
+        decimals: 0,
+        negativeValueFormat: NegativeValueFormat.PrefixDash,
+        removeTrailingZeroes: false,
+        toSeparateThousands: true,
+        unit: Unit.Auto,
+    };
+    const formatConfig: FormatConfig = {
+        __typename: 'FormatConfig',
+        category: CategoryType.Number,
+        isCategoryEditable: true,
+        numberFormatConfig,
+        percentageFormatConfig: null,
+    };
+    return formatConfig;
+};
+
+/**
+ * Maps configuration details to a formatter configuration object.
+ *
+ * @param absFloatValue - The absolute value of the number being formatted.
+ * @param configDetails - The number or currency format configuration.
+ * @returns A mapped formatter configuration.
+ */
+export const mapFormatterConfig = (
+    absFloatValue: number,
+    configDetails: NumberFormatConfig | CurrencyFormatConfig,
+): FormatterConfig => {
+    if (typeof configDetails.unit === 'number') {
+        // eslint-disable-next-line no-param-reassign
+        configDetails.unit = PROTO_TO_UNITS[configDetails.unit];
+    }
+    const isAutoFormatted = configDetails.unit === Unit.Auto;
+    let unitDetails = configDetails.unit || Unit.Auto;
+    let decimalDetails = configDetails.decimals || 0;
+
+    if (isAutoFormatted) {
+        unitDetails = getAutoUnit(absFloatValue);
+        if (unitDetails !== Unit.None) {
+            decimalDetails = DEFAULT_DECIMAL_PRECISION;
+        }
+    }
+    const shouldRemoveTrailingZeros =
+        configDetails.removeTrailingZeroes || isAutoFormatted;
+    return {
+        unitDetails,
+        decimalDetails,
+        shouldRemoveTrailingZeros,
+    };
+};
+
+/**
+ * Handles formatting of special data values (e.g., null, empty, NaN, Infinity).
+ *
+ * @param value - The value to check and format.
+ * @returns A formatted special value or null if not applicable.
+ */
+export function formatSpecialDataValue(value: any) {
+    if (
+        value === strings.NULL_VALUE_PLACEHOLDER_LABEL ||
+        value === strings.EMPTY_VALUE_PLACEHOLDER_LABEL
+    ) {
+        return value;
+    }
+
+    if (value === null || value === undefined) {
+        return strings.NULL_VALUE_PLACEHOLDER_LABEL;
+    }
+    // {Empty} placeholder is set for empty string or no characters
+    // other than spaces.
+    if (value === '') {
+        return strings.EMPTY_VALUE_PLACEHOLDER_LABEL;
+    }
+    if (value instanceof Array) {
+        if (!value.length || value[0] === null || value[0] === undefined) {
+            return strings.NULL_VALUE_PLACEHOLDER_LABEL;
+        }
+
+        switch (value[0]) {
+            case 'NaN':
+                return 'NaN';
+            case 'Infinity':
+                return 'Infinity';
+            default:
+                return null;
+        }
+    }
+
+    return null;
+}
---
+++ src/utils/number-formatting/globalize-setup.ts
@@ -0,0 +1,213 @@
+/**
+ * @file: Initialize Globalize
+ *
+ * @author Yashvardhan Nehra <yashvardhan.nehra@thoughtspot.com>
+ *
+ * Copyright: ThoughtSpot Inc. 2024
+ */
+
+/* eslint-disable import/no-extraneous-dependencies */
+import enCaGregorian from 'cldr-data/main/en/ca-gregorian.json';
+import enCurrencies from 'cldr-data/main/en/currencies.json';
+import enNumbers from 'cldr-data/main/en/numbers.json';
+import currencyData from 'cldr-data/supplemental/currencyData.json';
+import supplemental from 'cldr-data/supplemental/likelySubtags.json';
+import enpluralJson from 'cldr-data/supplemental/plurals.json';
+import Globalize from 'globalize';
+import _ from 'lodash';
+
+let currentLocale: string;
+let currentCurrencyFormat: any;
+let supplementalCurrencyDataJson: any;
+
+/**
+ * Extracts the country code from a locale string.
+ *
+ * @param locale - The locale string (e.g., 'en-US', 'en_US').
+ * @returns The country code (e.g., 'US') or the input locale if no delimiter is found.
+ */
+export const getCountryCode = (locale: string): string => {
+    let parts = locale.split('_'); //  // Split by '_'
+    if (parts.length === 2) {
+        return parts[1];
+    }
+    parts = locale.split('-'); //  // Split by '-'
+    if (parts.length === 2) {
+        return parts[1];
+    }
+    return locale;
+};
+
+/**
+ * Retrieves the default currency code for the current locale.
+ *
+ * @returns The default currency code (e.g., 'USD') or GBP if not found.
+ */
+export const getDefaultCurrencyCode = (): string => {
+    if (currentCurrencyFormat) {
+        return currentCurrencyFormat;
+    }
+
+    const countryCode = getCountryCode(currentLocale);
+    const regionData =
+        supplementalCurrencyDataJson?.supplemental?.currencyData?.region[
+            countryCode.toUpperCase()
+        ];
+    if (!regionData || regionData.length === 0) {
+        console.warn('No currency data found for country:', countryCode);
+        return 'GBP';
+    }
+    return Object.keys(regionData[regionData.length - 1])[0];
+};
+
+/**
+ * Sets the current locale for Globalize and updates the global state.
+ *
+ * @param locale - The locale string to set (e.g., 'en-US').
+ */
+export const setGlobalizeLocale = (locale: string): void => {
+    Globalize.locale(locale);
+    currentLocale = locale;
+};
+
+/**
+ * Retrieves the current locale set in Globalize.
+ *
+ * @returns The current locale string (e.g., 'en-US').
+ */
+export const getGlobalizeLocale = () => currentLocale;
+
+/**
+ * Updates the current currency format.
+ *
+ * @param currencyFormat - The currency format to set.
+ */
+export const setCurrentCurrencyFormat = (currencyFormat: any): void => {
+    currentCurrencyFormat = currencyFormat;
+};
+
+/**
+ * Retrieves the current currency format.
+ *
+ * @returns The current currency format.
+ */
+export const getCurrentCurrencyFormat = () => currentCurrencyFormat;
+
+/**
+ * Loads supplemental currency data for Globalize.
+ *
+ * @param data - The supplemental currency data to load.
+ */
+export const loadCurrencyData = (data: any) => {
+    supplementalCurrencyDataJson = data;
+};
+
+/**
+ * Loads CLDR data into Globalize.
+ *
+ * @param data - The CLDR data to load.
+ */
+export const loadGlobalizeData = (data: any) => {
+    Globalize.load(data);
+};
+
+/**
+ * Initializes Globalize with CLDR data and sets the default locale.
+ *
+ * @param locale - The locale to initialize Globalize with (default: 'en-gb').
+ */
+export const initializeGlobalize = (locale = 'en-gb') => {
+    loadGlobalizeData(enNumbers);
+    loadGlobalizeData(enCaGregorian);
+    loadGlobalizeData(supplemental);
+    loadGlobalizeData(currencyData);
+    loadGlobalizeData(enpluralJson);
+    loadGlobalizeData(enCurrencies);
+
+    loadCurrencyData(currencyData);
+
+    setGlobalizeLocale(locale);
+};
+
+/**
+ * Creates a number formatter with the given options.
+ *
+ * @param format - The Globalize number formatter options.
+ * @returns A formatter function for numbers.
+ */
+export function globalizeNumberFormatter(
+    format: Globalize.NumberFormatterOptions,
+): (num: number) => string {
+    return Globalize.numberFormatter(format);
+}
+
+/**
+ * Creates a currency formatter with the given options.
+ *
+ * @param currencyCode - The ISO currency code (e.g., 'USD').
+ * @param format - The Globalize currency formatter options.
+ * @returns A formatter function for currency values.
+ */
+export function globalizeCurrencyFormatter(
+    currencyCode: string,
+    format: Globalize.CurrencyFormatterOptions,
+): (num: number) => string {
+    return Globalize.currencyFormatter(currencyCode, format);
+}
+
+/**
+ * Formats a number using Globalize, handling errors gracefully.
+ *
+ * @param format - Globalize number formatter options.
+ * @param num - The number to format.
+ * @returns The formatted number as a string.
+ */
+export function formatNumberSafely<
+    FormatOptions extends Globalize.NumberFormatterOptions
+>(format: FormatOptions, num: number): string {
+    try {
+        const formatter = globalizeNumberFormatter(format);
+        const formattedNumber = formatter(num);
+        return formattedNumber;
+    } catch (e) {
+        console.error('Error formatting pattern: ', format, num, e);
+        if (Math.abs(num) < 1e-7) {
+            return '0';
+        }
+        return String(num);
+    }
+}
+
+/**
+ * Sanitizes a number format to ensure compatibility with Globalize.
+ *
+ * @param format - The raw format string (e.g., '#.##').
+ * @returns A sanitized format string (e.g., '0.##').
+ */
+export const sanitizeFormat = (format: string): string => {
+    // Globalize needs to have a zero before the decimal point
+    // or at the end of format if no decimal point
+    let sanitizedFormat = format.replace(/#\./, '0.');
+    if (!sanitizedFormat.includes('.')) {
+        sanitizedFormat = sanitizedFormat.replace(/#(%?)$/, '0$1');
+    }
+    return sanitizedFormat;
+};
+
+/**
+ * Validates if a given number format is compatible with Globalize.
+ *
+ * @param format - The raw format string.
+ * @returns True if the format is valid; otherwise, false.
+ */
+export const validateNumberFormat = (format: string): boolean => {
+    try {
+        Globalize.numberFormatter({
+            ...({ raw: sanitizeFormat(format) } as any),
+        })(123); // Test the formatter with a dummy value
+    } catch (e) {
+        console.error('Invalid number format:', format, e);
+        return false;
+    }
+    return true;
+};
