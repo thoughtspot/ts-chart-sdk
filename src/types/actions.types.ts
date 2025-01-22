626bf957435f1a114aff57c0a25525105e6e2818
harshmeet.singh <harshmeet.singh@thoughtspot.com>
2025-01-22 12:39:11 +0530

    update chartconfigparameters

--- src/types/actions.types.ts
+++ src/types/actions.types.ts
@@ -3,7 +3,7 @@
  * Defines enum for All action types supported in TS, includes context menu actions,
  * answer actions liveboard actions and axis menu actions
  * @author Harshmeet Singh <harshmeet.singh@thoughtspot.com>
- * SDK @version 0.0.2-alpha.21
+ * SDK @version 0.0.2-alpha.26
  */
 
 export enum Action {
